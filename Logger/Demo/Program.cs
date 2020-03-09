a2879caf037f9a6b14db4d0564435311662c26f9
Andrew Wagner <38209339+andrewwagnerkg@users.noreply.github.com>
2020-03-09 21:31:01 +0600

    Add demo app

---
+++ Logger/Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿using Logger;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IFileLogger logger = new FileLogger();
+            //logger.FileSize = 2; // Rotation file size 2 MB by default
+            //logger.Path = Directory.GetCurrentDirectory(); //path for log storage by default current directory
+            for (int i = 0; i < 10000; i++)
+            {
+                logger.Write<Program>($"string for example. Write iterator value {i}");
+            }
+        }
+    }
+}
